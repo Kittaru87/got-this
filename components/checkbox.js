3db3750e060e438bb3600d4d55ccaef85295add4
Sophia Bell <sophbell87@gmail.com>
2020-07-15 12:51:00 +0100

    increment counter works, setSelection in checkbox needs fixing

--- components/checkbox.js
+++ components/checkbox.js
@@ -3,13 +3,17 @@ import { StyleSheet, Text, View, CheckBox } from "react-native";
 
 export default function Checkbox(props) {
   const [isSelected, setSelection] = useState(false);
+  console.log(isSelected);
 
   return (
     <View style={styles.container}>
       <View style={styles.checkboxContainer}>
         <CheckBox
-          value={props.selected}
-          onValueChange={props.onValueChange}
+          value={isSelected}
+          onValueChange={() => {
+            setSelection;
+            props.incrementCount(isSelected);
+          }}
           style={styles.checkbox}
         />
         <Text style={styles.label}>{props.label}</Text>
--- components/fitness.js
+++ components/fitness.js
@@ -3,30 +3,41 @@ import Checkbox from "./checkbox";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Fitness() {
-  const [items, setItems] = useState([
-    { label: "I have gone for a walk today", selected: false },
-  ]);
+  // const [items, setItems] = useState(false);
+  const [count, setCount] = useState(0);
+  console.log(count);
 
-  const changeSelection = (item) => {
-    setItems((prevItems) => {
-      return prevItems.map((item) => {
-        (item.selected = !item.selected), (item.label = item.label);
-      });
-    });
+  const showCount = count;
+
+  // const itemsToggler = (isSelected) => {
+  //   isSelected === true ? setItems((items += 1)) : setItems((items -= 1));
+  //   console.log(items);
+  // };
+
+  const incrementCount = (isSelected) => {
+    isSelected ? setCount(count + 1) : setCount(count - 1);
   };
+
+  // const changeSelection = (item) => {
+  //   setItems((prevItems) => {
+  //     return prevItems.map((item) => {
+  //       (item.selected = !item.selected), (item.label = item.label);
+  //     });
+  //   });
+  // };
 
   return (
     <View style={styles.container}>
       <Text style={styles.h2}>Fitness</Text>
       <Checkbox
-        label={items[0].label}
-        onValueChange={changeSelection}
-        selected={items[0].selected}
+        label="I have gone for a walk today"
+        incrementCount={incrementCount}
       />
       <Checkbox label="I have done more than 30 mins exercise" />
       <Checkbox label="I have done more than an hour exercise" />
       <Checkbox label="I did some yoga today" />
       <Checkbox label="I went for a run" />
+      {showCount}
     </View>
   );
 }

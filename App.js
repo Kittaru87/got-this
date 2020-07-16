42d62b408b9c9946741e72f2941888e92faf8775
Sophia Bell <sophbell87@gmail.com>
2020-07-16 14:59:45 +0100

    adding in nav/header component

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import Header from "./components/header";
 import Self from "./components/self";
 import Play from "./components/play";
 import Living from "./components/living";
 import Work from "./components/work";
 import { StyleSheet, Text, ScrollView, SafeAreaView } from "react-native";
-import Constants from 'expo-constants';
+import Constants from "expo-constants";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-    <ScrollView style={styles.scrollView}>
-      <Text>Got This</Text>
-      <Text>Hello</Text>
-      <Self />
-      <Play />
-      <Living />
-      <Work />
-      <StatusBar style="auto" />
-    </ScrollView>
+      <ScrollView style={styles.scrollView}>
+        <Header />
+        <Self />
+        <Play />
+        <Living />
+        <Work />
+        <StatusBar style="auto" />
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -29,6 +29,6 @@ const styles = StyleSheet.create({
     marginTop: Constants.statusBarHeight,
   },
   scrollView: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
 });

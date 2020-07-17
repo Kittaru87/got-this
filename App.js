43b8b8e8c34a4f8272022bb3a24b94063be2b845
Gareth Harris <gdharris1000@gmail.com>
2020-07-17 11:00:52 +0100

    added acheivement list and addAcheivement function

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, {useState} from "react";
 import Self from "./components/self";
 import Play from "./components/play";
 import Living from "./components/living";
@@ -8,6 +8,36 @@ import { StyleSheet, Text, ScrollView, SafeAreaView } from "react-native";
 import Constants from 'expo-constants';
 
 export default function App() {
+
+  const [achievements, setAchievements] = useState([
+    {achievement: 'I have gone for a walk today', category: 'Self'},
+    {achievement: 'I have done more than 30 mins exercise', category: 'Self'},
+    {achievement: 'I have done more than an hour exercise', category: 'Self'},
+    {achievement: 'I have meditated', category: 'Self'},
+    {achievement: 'I had a nice bath', category: 'Self'},
+    {achievement: 'Filed expenses', category: 'Work'},
+    {achievement: 'Set out of office email', category: 'Work'},
+    {achievement: 'Didn\'t steal any pens', category: 'Work'},
+    {achievement: 'Booked holiday', category: 'Work'},
+    {achievement: 'Won employee of the month', category: 'Work'},
+    {achievement: 'Mario Kart', category: 'Play'},
+    {achievement: 'Monopoly', category: 'Play'},
+    {achievement: 'Chess', category: 'Play'},
+    {achievement: 'Darts', category: 'Play'},
+    {achievement: 'Lots and lots of television', category: 'Play'},
+    {achievement: 'Paid bills', category: 'Living'},
+    {achievement: 'Cleaned kitchen', category: 'Living'},
+    {achievement: 'Waterd plants', category: 'Living'},
+    {achievement: 'Vaccumed house', category: 'Living'},
+    {achievement: 'Taken out rubbish', category: 'Living'},
+  ]);
+
+  const addItem = (achievement, category) => {
+    setItems((prevItems) => {
+      return [{achievement, category }, ...prevItems];
+    });
+  }
+
   return (
     <SafeAreaView style={styles.container}>
     <ScrollView style={styles.scrollView}>
